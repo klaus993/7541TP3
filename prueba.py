2d4e766e7f0b33c698ae4e86f6d413de99a26884
Ramiro <you@example.com>
2016-12-07 05:00:18 -0300

    SIgo intentando hcaer distancias

--- prueba.py
+++ prueba.py
@@ -8,20 +8,22 @@ def contador(dic, indice):
 	dic[indice] = "caca"
 
 def generar_distancias(v, padre, orden, distancias):
-	if padre != distancias[0][0]:
-		distancias[1][v] = {}
-		distancias[1][v][padre] = (, 0) 
-		distancias[0][1] += 1
+	if not padre:
+		distancias[1][0] = 0 #diccionario distancia: contador
+		return
+	elif padre != distancias[0][0]:
+		if distancias[0][0] not in distancias[1]:
+			distancias[1][distancias[0][0]] = 0
+			distancias[0][1] += 1
 		distancias[0][0] = padre
-	if padre != None:
-		distancias[1][distancias[0][1]] += 1
+	distancias[1][distancias[0][1] - 1] += 1
 
 def distancias(grafo, personaje):
 	''' Recibe un grafo y el identificador de un vértice. Imprime por pantalla
 	la cantidad de personajes que se encuentran a cada una de las distancias
 	posibles. '''
 	dic_distancias = {}
-	distancias = [[0, 1], dic_distancias] 
+	distancias = [[None, 1], dic_distancias] 
 	grafo.recorrer("bfs", generar_distancias, distancias, personaje)
 	for clave,valor in distancias[1].items():
 		print("Distancia "+ str(clave)+": "+str(valor))
